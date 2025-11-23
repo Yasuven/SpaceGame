ef262cd1fa022ef413776828a5656375b8147127
DannyMcNuggets <dannymcwhopper@gmail.com>
2025-11-23 22:06:12 +0200

    planet 1 condition fix

--- Assets/Resources/PlanetConditions/Planet1_conditions.cs
+++ Assets/Resources/PlanetConditions/Planet1_conditions.cs
@@ -5,12 +5,6 @@ public class Planet1Condition : PlanetCondition
 {
     public override int VerifyConditions(Planet planet)
     {
-        if (DataCarrier.points >= 1000)
-        {
-            Debug.Log("we are returning 3 points from conditions");
-             return 3;
-        }
-
         return -1;
     }
 }
