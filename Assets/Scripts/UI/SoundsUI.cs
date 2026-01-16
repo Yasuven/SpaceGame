6379ca600994ac755eaa53b96cfbf45c6a1120c0
Korvits <marek.kalda5@gmail.com>
2026-01-16 18:23:40 +0200

    Plays audioclip when using keyboard in OptionsMenu

--- Assets/Scripts/UI/SoundsUI.cs
+++ Assets/Scripts/UI/SoundsUI.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class UISound : MonoBehaviour, IPointerClickHandler
+public class UISound : MonoBehaviour, IPointerClickHandler, ISubmitHandler
 {
     public AudioClip clickClip;
 
     public void OnPointerClick(PointerEventData eventData)
+    {
+        if (clickClip != null)
+            AudioManager.Instance.PlaySound(clickClip);
+    }
+
+    public void OnSubmit(BaseEventData eventData)
     {
         if (clickClip != null)
             AudioManager.Instance.PlaySound(clickClip);
