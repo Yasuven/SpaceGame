482706fa1605e676b2db45f98f11431ec8e2d4d0
Korvits <marek.kalda5@gmail.com>
2026-01-17 20:43:43 +0200

    Hotfix for saving controls

--- Assets/Samples/Input System/1.14.2/Rebinding UI/RebindActionUI.cs
+++ Assets/Samples/Input System/1.14.2/Rebinding UI/RebindActionUI.cs
@@ -365,15 +365,20 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
             LoadActionBinding();
         }
 
+        private void Start()
+        {
+            LoadActionBinding();
+        }
+
         private void SaveActionBinding()
         {
             var currentBindings = actionReference.action.actionMap.SaveBindingOverridesAsJson();
-            PlayerPrefs.SetString(m_Action.action.name + bindingId, currentBindings);
+            PlayerPrefs.SetString("controls", currentBindings);
         }
 
         private void LoadActionBinding()
         {
-            var savedBindings = PlayerPrefs.GetString(m_Action.action.name + bindingId);
+            var savedBindings = PlayerPrefs.GetString("controls");
             if (!string.IsNullOrEmpty(savedBindings)) { 
                 m_Action.action.actionMap.LoadBindingOverridesFromJson(savedBindings);
                 UpdateBindingDisplay();
